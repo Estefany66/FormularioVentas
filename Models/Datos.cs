0f5dc25006fb0398c3b811a0dde6514faa6411a7
Estefany Calatayud <estefany_calatayud@usmp.pe>
2021-03-25 08:27:45 -0500

    Desarrollo

---
+++ Models/Datos.cs
@@ -0,0 +1,12 @@
+using System;
+namespace formventas.Models{
+    public class Datos{
+        public string DatosdeProducto {get ; set; }
+        public string Nombre {get ; set; }
+        public double Cantidad {get ; set; }
+        public double Precio {get ; set; }
+     
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace formventas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
